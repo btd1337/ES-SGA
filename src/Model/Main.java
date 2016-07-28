ccf5a751542b7bc377bc72bb40a0834f370fb673
btd1337 <helder.bertoldo@ice.ufjf.br>
2016-07-28 11:50:09 -0300

    SGA 0.14
    Menu de Opções: Criado e Referenciado

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -5,6 +5,9 @@
  */
 package Model;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author btd1337
@@ -15,7 +18,69 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        int opMenu;
+        
+        do{
+            exibeMenu();
+            try{
+                opMenu = scanner.nextInt();
+                getOpcaoEscolhida(opMenu);
+            }catch(InputMismatchException ex){
+                System.out.println("Valor inválido!");
+                opMenu = -1;
+            }
+            
+        }while(opMenu != 0);
+        
+    }
+
+    private static void exibeMenu() {
+         System.out.println("1 - Criar Departamento");
+         System.out.println("2 - Listar Departamentos");
+         System.out.println("3 - Criar Curso");
+         System.out.println("4 - Listar Cursos");
+         System.out.println("5 - Criar Professor");
+         System.out.println("6 - Listar Professores");
+         System.out.println("0 - Sair");
+         System.out.print("Op: ");
+    }
+
+    private static void getOpcaoEscolhida(int opMenu) {
+        switch(opMenu){
+            case 1 : {
+                criaDepartamento();
+                break;
+            }
+            case 2 : {
+                listaDepartamentos();
+                break;
+            }
+            case 3 : {
+                criaCurso();
+                break;
+            }
+            case 4 : {
+                listaCursos();
+                break;
+            }
+            case 5 : {
+                criaProfessor();
+                break;
+            }
+            case 6 : {
+                listaProfessores();
+                break;
+            }
+            case 0 : {
+                System.out.println("\nSistema Encerrado!");
+                break;
+            }
+            default:{
+                System.out.println("Opção Inválida");
+            }
+        }
     }
     
 }
